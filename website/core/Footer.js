36eacdc48ec3f6c9cc33f0d9b1c4c75013a760e7
강동윤 <kdy1@outlook.kr>
2019-02-11 18:59:04 +0900

    Fix footer links

--- website/core/Footer.js
+++ website/core/Footer.js
@@ -12,7 +12,8 @@ class Footer extends React.Component {
     const baseUrl = this.props.config.baseUrl;
     const docsUrl = this.props.config.docsUrl;
     const docsPart = `${docsUrl ? `${docsUrl}/` : ''}`;
-    const langPart = `${language ? `${language}/` : ''}`;
+    // const langPart = `${language ? `${language}/` : ''}`;
+    const langPart = '';
     return `${baseUrl}${docsPart}${langPart}${doc}`;
   }
 
