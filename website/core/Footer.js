20ced64499ddc40e0df6f749da4c06df29bbd7fb
강동윤 <kdy1997.dev@gmail.com>
2020-01-05 00:33:13 +0900

    Slack instead of gitter

--- website/core/Footer.js
+++ website/core/Footer.js
@@ -5,21 +5,21 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-const React = require('react');
+const React = require("react");
 
 class Footer extends React.Component {
   docUrl(doc, language) {
     const baseUrl = this.props.config.baseUrl;
     const docsUrl = this.props.config.docsUrl;
-    const docsPart = `${docsUrl ? `${docsUrl}/` : ''}`;
+    const docsPart = `${docsUrl ? `${docsUrl}/` : ""}`;
     // const langPart = `${language ? `${language}/` : ''}`;
-    const langPart = '';
+    const langPart = "";
     return `${baseUrl}${docsPart}${langPart}${doc}`;
   }
 
   pageUrl(doc, language) {
     const baseUrl = this.props.config.baseUrl;
-    return baseUrl + (language ? `${language}/` : '') + doc;
+    return baseUrl + (language ? `${language}/` : "") + doc;
   }
 
   render() {
@@ -37,31 +37,37 @@ class Footer extends React.Component {
           </a>
           <div>
             <h5>Docs</h5>
-            <a href={this.docUrl('installation.html', this.props.language)}>
+            <a href={this.docUrl("installation.html", this.props.language)}>
               Installation
             </a>
-            <a href={this.docUrl('configuring-swc.html', this.props.language)}>
+            <a href={this.docUrl("configuring-swc.html", this.props.language)}>
               Configuring swc
             </a>
-            <a href={this.docUrl('usage-cli.html', this.props.language)}>
+            <a href={this.docUrl("usage-cli.html", this.props.language)}>
               Usage
             </a>
-            <a href={this.docUrl('migrating-from-babel-core.html', this.props.language)}>
+            <a
+              href={this.docUrl(
+                "migrating-from-babel-core.html",
+                this.props.language
+              )}
+            >
               Migration
             </a>
           </div>
           <div>
             <h5>Community</h5>
-            <a href={this.pageUrl('users.html', this.props.language)}>
+            <a href={this.pageUrl("users.html", this.props.language)}>
               User Showcase
             </a>
             <a
               href="http://stackoverflow.com/questions/tagged/swc-compiler"
               target="_blank"
-              rel="noreferrer noopener">
+              rel="noreferrer noopener"
+            >
               Stack Overflow
             </a>
-            <a href="https://gitter.im/swcproject/Lobby">Chat on gitter</a>
+            <a href="https://swc-slackin.herokuapp.com/">Slackin for swc</a>
             {/* <a
               href="https://twitter.com/"
               target="_blank"
@@ -80,7 +86,8 @@ class Footer extends React.Component {
               data-count-href="/facebook/docusaurus/stargazers"
               data-show-count="true"
               data-count-aria-label="# stargazers on GitHub"
-              aria-label="Star this project on GitHub">
+              aria-label="Star this project on GitHub"
+            >
               Star
             </a>
           </div>
