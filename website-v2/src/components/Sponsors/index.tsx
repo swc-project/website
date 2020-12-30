02e3c9e737a7e216d13efa249cbbc74a9a3c1d3d
Martin Feckie <mfeckie@gmail.com>
2020-12-30 20:17:44 +0800

    Style sponsors

--- website-v2/src/components/Sponsors/index.tsx
+++ website-v2/src/components/Sponsors/index.tsx
@@ -1,39 +1,32 @@
 import React from "react";
 import { LinkButton } from "../LinkButton";
+import styles from "../../pages/common.module.css";
 
 export function Sponsors() {
   return (
-    <div className="productShowcaseSection paddingBottom">
-      <div>
-        <h2 id="gold-sponsers" align="center">
-          Gold Sponsors
-        </h2>
+    <div>
+      <div className={styles.flexColumnCenter}>
+        <h2>Gold Sponsors</h2>
         <a href="https://opencollective.com/swc">
           <img src="https://opencollective.com/swc/tiers/gold-sponsers.svg?avatarHeight=64" />
         </a>
 
-        <h2 id="silver-sponsers" align="center">
-          Silver Sponsors
-        </h2>
+        <h2>Silver Sponsors</h2>
         <a href="https://opencollective.com/swc">
           <img src="https://opencollective.com/swc/tiers/silver-sponsers.svg?avatarHeight=64" />
         </a>
 
-        <h2 id="bronze-sponsers" align="center">
-          Bronze Sponsors
-        </h2>
+        <h2>Bronze Sponsors</h2>
         <a href="https://opencollective.com/swc">
           <img src="https://opencollective.com/swc/tiers/bronze-sponsers.svg?avatarHeight=64" />
         </a>
 
-        <h2 id="backers" align="center">
-          Backers
-        </h2>
+        <h2>Backers</h2>
         <a href="https://opencollective.com/swc">
           <img src="https://opencollective.com/swc/tiers/backer.svg?avatarHeight=64" />
         </a>
       </div>
-      <div className="more-users">
+      <div className={`padding-vert--lg ${styles.flexRowCenter}`}>
         <LinkButton href="https://opencollective.com/swc">Donate</LinkButton>
       </div>
     </div>
