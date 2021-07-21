bd75c97d6658e12555916285ad0d1436fb9fd268
강동윤 <kdy1997.dev@gmail.com>
2021-07-21 14:42:42 +0900

    fmt

--- website/src/components/Repl/index.tsx
+++ website/src/components/Repl/index.tsx
@@ -1,50 +1,70 @@
-import React, { ComponentPropsWithoutRef, ComponentPropsWithRef, MutableRefObject, useCallback, useEffect, useRef, useState } from 'react';
-import type { Controlled as CodeMirror, UnControlled as UnControlledCodeBlock } from 'react-codemirror2';
-import { sampleCode } from './sampleCode';
+import React, {
+  ComponentPropsWithoutRef,
+  ComponentPropsWithRef,
+  MutableRefObject,
+  useCallback,
+  useEffect,
+  useRef,
+  useState,
+} from "react";
+import type {
+  Controlled as CodeMirror,
+  UnControlled as UnControlledCodeBlock,
+} from "react-codemirror2";
+import { sampleCode } from "./sampleCode";
 
 const DEBOUNCE_MS = 500;
-const codeMirrorOptions: ComponentPropsWithoutRef<typeof CodeMirror>['options'] = {
-  mode: { name: 'javascript', typescript: true },
+const codeMirrorOptions: ComponentPropsWithoutRef<
+  typeof CodeMirror
+>["options"] = {
+  mode: { name: "javascript", typescript: true },
   lineNumbers: true,
   lineWrapping: true,
 };
-const noop = () => { };
+const noop = () => {};
 
 export const Repl = () => {
   const [rawCode, setRawCode] = useState(sampleCode);
-  const [loadingImportsStatus, setLoadingImportsStatus] = useState<'loading' | 'success' | 'fail'>('loading');
+  const [loadingImportsStatus, setLoadingImportsStatus] = useState<
+    "loading" | "success" | "fail"
+  >("loading");
   const [compiledCode, setCompiledCode] = useState(`const test = 1`);
   const [compileError, setCompileError] = useState<null | string>(null);
   const compileTimeout = useRef<number | null>(null);
 
-  const swcWasm: MutableRefObject<typeof import('@swc/wasm-web') | null> = useRef(null);
-  const CodeMirrorLazyImported: MutableRefObject<(typeof import('react-codemirror2')) | null> = useRef(null);
-  const RawCodeEditor: MutableRefObject<CodeMirror.Editor | null> = useRef(null);
-  const CompiledCodeEditor: MutableRefObject<CodeMirror.Editor | null> = useRef(null);
+  const swcWasm: MutableRefObject<typeof import("@swc/wasm-web") | null> =
+    useRef(null);
+  const CodeMirrorLazyImported: MutableRefObject<
+    typeof import("react-codemirror2") | null
+  > = useRef(null);
+  const RawCodeEditor: MutableRefObject<CodeMirror.Editor | null> =
+    useRef(null);
+  const CompiledCodeEditor: MutableRefObject<CodeMirror.Editor | null> =
+    useRef(null);
   const [editorsLoadedCount, setEditorsLoadedCount] = useState(0);
 
   useEffect(() => {
     async function importAndRunSwcOnMount() {
       try {
         [swcWasm.current, CodeMirrorLazyImported.current] = await Promise.all([
-          import('@swc/wasm-web'),
-          import('react-codemirror2'),
-          import('codemirror/mode/javascript/javascript'),
-          import('../../../node_modules/codemirror/lib/codemirror.css'),
+          import("@swc/wasm-web"),
+          import("react-codemirror2"),
+          import("codemirror/mode/javascript/javascript"),
+          import("../../../node_modules/codemirror/lib/codemirror.css"),
         ]);
 
         const importWasm = swcWasm.current.default();
         await importWasm;
-        setLoadingImportsStatus('success');
+        setLoadingImportsStatus("success");
       } catch (e) {
-        setLoadingImportsStatus('fail');
+        setLoadingImportsStatus("fail");
       }
     }
     importAndRunSwcOnMount();
   }, []);
 
   useEffect(() => {
-    if ('success' !== loadingImportsStatus || !swcWasm.current) return;
+    if ("success" !== loadingImportsStatus || !swcWasm.current) return;
 
     compileTimeout.current = window.setTimeout(() => {
       try {
@@ -60,16 +80,22 @@ export const Repl = () => {
     };
   }, [loadingImportsStatus, rawCode]);
 
-  const onRawCodeChange: ComponentPropsWithRef<typeof CodeMirror>['onBeforeChange'] = useCallback((_editor, _data, value) => {
+  const onRawCodeChange: ComponentPropsWithRef<
+    typeof CodeMirror
+  >["onBeforeChange"] = useCallback((_editor, _data, value) => {
     setRawCode(value);
   }, []);
 
-  const onRawCodeEditorDidMount: ComponentPropsWithRef<typeof CodeMirror>['editorDidMount'] = useCallback((editor) => {
+  const onRawCodeEditorDidMount: ComponentPropsWithRef<
+    typeof CodeMirror
+  >["editorDidMount"] = useCallback((editor) => {
     CompiledCodeEditor.current = editor;
     setEditorsLoadedCount((prev) => prev + 1);
   }, []);
 
-  const onCompiledCodeEditorDidMount: ComponentPropsWithRef<typeof CodeMirror>['editorDidMount'] = useCallback((editor) => {
+  const onCompiledCodeEditorDidMount: ComponentPropsWithRef<
+    typeof CodeMirror
+  >["editorDidMount"] = useCallback((editor) => {
     RawCodeEditor.current = editor;
     setEditorsLoadedCount((prev) => prev + 1);
   }, []);
@@ -85,39 +111,43 @@ export const Repl = () => {
     });
   }, [editorsLoadedCount]);
 
-  return <section>
-    {(() => {
-      switch (loadingImportsStatus) {
-        case `loading`:
-          return <h2>Please wait. Loading wasm and js files...</h2>;
-        case `fail`:
-          return <h2>Loading wasm and js files failed. Please try again.</h2>;
-        case `success`:
-          return <article
-            style={{
-              display: 'flex',
-            }}
-          >
-            <div style={{ width: `100%` }}>
-              <h2>Your code</h2>
-              <CodeMirrorLazyImported.current.Controlled
-                value={rawCode}
-                options={codeMirrorOptions}
-                onBeforeChange={onRawCodeChange}
-                onChange={noop}
-                editorDidMount={onRawCodeEditorDidMount}
-              />
-            </div>
-            <div style={{ width: `100%` }}>
-              <h2>Compiled code</h2>
-              <CodeMirrorLazyImported.current.UnControlled
-                value={compileError ?? compiledCode}
-                options={codeMirrorOptions}
-                editorDidMount={onCompiledCodeEditorDidMount}
-              />
-            </div>
-          </article>;
-      }
-    })()}
-  </section>;
+  return (
+    <section>
+      {(() => {
+        switch (loadingImportsStatus) {
+          case `loading`:
+            return <h2>Please wait. Loading wasm and js files...</h2>;
+          case `fail`:
+            return <h2>Loading wasm and js files failed. Please try again.</h2>;
+          case `success`:
+            return (
+              <article
+                style={{
+                  display: "flex",
+                }}
+              >
+                <div style={{ width: `100%` }}>
+                  <h2>Your code</h2>
+                  <CodeMirrorLazyImported.current.Controlled
+                    value={rawCode}
+                    options={codeMirrorOptions}
+                    onBeforeChange={onRawCodeChange}
+                    onChange={noop}
+                    editorDidMount={onRawCodeEditorDidMount}
+                  />
+                </div>
+                <div style={{ width: `100%` }}>
+                  <h2>Compiled code</h2>
+                  <CodeMirrorLazyImported.current.UnControlled
+                    value={compileError ?? compiledCode}
+                    options={codeMirrorOptions}
+                    editorDidMount={onCompiledCodeEditorDidMount}
+                  />
+                </div>
+              </article>
+            );
+        }
+      })()}
+    </section>
+  );
 };
