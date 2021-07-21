efada5ec60cebb7c1bf5e994a5869eeb060cb5c5
강동윤 <kdy1997.dev@gmail.com>
2021-07-21 19:52:50 +0900

    Better ui

--- website/src/pages/repl.tsx
+++ website/src/pages/repl.tsx
@@ -1,14 +1,18 @@
 import React from "react";
 
+import styles from "../pages/common.module.css";
 import Layout from "@theme/Layout";
 import { Repl } from "../components/Repl";
 
 export default (props) => (
   <Layout>
-    <p>
-      Because swc can be easily compiled into wasm, it can also run on the web.
-      You can try it out here.
-    </p>
+    <div className={`padding-vert--lg ${styles.flexColumnCenter}`}>
+      <p>
+        Because swc can be easily compiled into wasm, it can also run on the
+        web. You can try it out here.
+      </p>
+    </div>
+
     <Repl />
   </Layout>
 );
