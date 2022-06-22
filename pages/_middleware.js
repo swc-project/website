341d601df85ce75279a0257aa580e4a126b42f1b
Guilherme Sousa <guilherssousa@gmail.com>
2022-06-22 13:05:54 -0300

    Add middleware exception for playground

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -1,2 +1,10 @@
+import { NextResponse } from "next/server";
 import { locales } from "nextra/locales";
-export const middleware = locales;
+
+export const middleware = (req) => {
+  const { nextUrl } = req;
+
+  if (nextUrl.pathname === "/playground") return NextResponse.next();
+
+  return locales(req);
+};
