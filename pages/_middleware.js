7083b7ece3e03b4a781e81eb9696d8e95d6eafa1
Guilherme Sousa <guilherssousa@gmail.com>
2022-06-22 13:10:35 -0300

    add comments about playground on middleware

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -4,6 +4,9 @@ import { locales } from "nextra/locales";
 export const middleware = (req) => {
   const { nextUrl } = req;
 
+  // Playground component breaks when browsing to it.
+  // Since it's not a language dynamic page, we ignore
+  // the locales middleware and run with Next default one.
   if (nextUrl.pathname === "/playground") return NextResponse.next();
 
   return locales(req);
