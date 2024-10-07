0aaaf6611c2826adfeb6cfdb70981ef8b0870ebd
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-10-08 08:25:16 +0900

    Core team!

---
+++ components/TeamMembers.jsx
@@ -0,0 +1,62 @@
+import Link from "next/link";
+
+const members = [
+  {
+    name: "kdy1 / Donny",
+    link: "https://github.com/kdy1",
+    github: "kdy1",
+    role: "Lead Maintainer",
+  },
+  {
+    name: "OJ Kwon",
+    link: "https://github.com/kwonoj",
+    github: "kwonoj",
+    role: "Core Team",
+  },
+  {
+    name: "magic-akari",
+    link: "https://github.com/magic-akari",
+    github: "magic-akari",
+    role: "Core Team",
+  },
+  {
+    name: "Austaras",
+    link: "https://github.com/Austaras",
+    github: "Austaras",
+    role: "Core Team",
+  },
+  {
+    name: "CPunisher",
+    link: "https://github.com/CPunisher",
+    github: "CPunisher",
+    role: "Core Team",
+  },
+  {
+    name: "LongYinan",
+    link: "https://github.com/Brooooooklyn",
+    github: "Brooooooklyn",
+    role: "Maintainer of @swc-node/*",
+  },
+];
+
+export default function TeamMembers() {
+  return (
+    <div className="grid grid-cols-3 gap-4 mt-4">
+      {members.map((member, index) => {
+        return (
+          <Link href={`https://github.com/${member.github}`} key={index}>
+            <div className="flex flex-col items-center">
+              <img
+                src={`https://github.com/${member.github}.png`}
+                alt={`${member.name} avatar`}
+                className="w-16 h-16 rounded-full"
+              />
+              <div className="font-bold text-center">{member.name}</div>
+              <div>{member.role}</div>
+            </div>
+          </Link>
+        );
+      })}
+    </div>
+  );
+}
