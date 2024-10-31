38d9e054ab1c4115f3feda782fabc5e5329e7649
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-10-31 11:14:23 +0900

    Make from unique

--- apps/plugins/app/import/ranges/page.tsx
+++ apps/plugins/app/import/ranges/page.tsx
@@ -2,45 +2,42 @@ import { db } from "@/lib/prisma";
 import fs from "node:fs/promises";
 
 export default async function Page() {
-    if (process.env.NODE_ENV === "production") {
-        return <div>Not allowed</div>;
-    }
+  if (process.env.NODE_ENV === "production") {
+    return <div>Not allowed</div>;
+  }
 
-    const ranges: { min: string; max: string }[] = JSON.parse(
-        await fs.readFile("./data/ranges.json", "utf8")
-    );
+  const ranges: { min: string; max: string }[] = JSON.parse(
+    await fs.readFile("./data/ranges.json", "utf8")
+  );
 
-    for (const { min, max } of ranges) {
-        await db.compatRange.upsert({
-            where: {
-                from_to: {
-                    from: min,
-                    to: max,
-                },
-            },
-            update: {},
-            create: {
-                from: min,
-                to: max,
-            },
-        });
-    }
+  for (const { min, max } of ranges) {
+    await db.compatRange.upsert({
+      where: {
+        from: min,
+      },
+      update: {},
+      create: {
+        from: min,
+        to: max,
+      },
+    });
+  }
 
-    const runtimes = ["@swc/core", "next", "rspack", "farm"];
+  const runtimes = ["@swc/core", "next", "rspack", "farm"];
 
-    for (const runtime of runtimes) {
-        await db.swcRuntime.upsert({
-            where: {
-                name: runtime,
-            },
-            update: {},
-            create: {
-                name: runtime,
-            },
-        });
-    }
+  for (const runtime of runtimes) {
+    await db.swcRuntime.upsert({
+      where: {
+        name: runtime,
+      },
+      update: {},
+      create: {
+        name: runtime,
+      },
+    });
+  }
 
-    return <div>Done</div>;
+  return <div>Done</div>;
 }
 
 export const dynamic = "force-dynamic";
