40ace3ff0a1f62f0956821a008d5a9828204ca83
강동윤 (Donny) <kdy1997.dev@gmail.com>
2025-04-11 14:14:36 +0900

    tailwind v4

--- apps/plugins/components/table-container.tsx
+++ apps/plugins/components/table-container.tsx
@@ -2,28 +2,28 @@ import { cn } from "@/lib/utils";
 import type { FC, ReactNode } from "react";
 
 type TableContainerProperties = {
-    readonly title: string;
-    readonly children: ReactNode;
-    readonly className?: string;
+  readonly title: string;
+  readonly children: ReactNode;
+  readonly className?: string;
 };
 
 export const TableContainer: FC<TableContainerProperties> = ({
-    title,
-    children,
-    className,
+  title,
+  children,
+  className,
 }) => (
-    <div className="bg-secondary flex flex-col rounded-2xl p-1">
-        <p className="text-foreground m-0 block shrink-0 px-4 py-2 text-sm font-medium">
-            {title}
-        </p>
-        <div
-            className={cn(
-                "relative flex-1 overflow-hidden rounded-xl border shadow-sm",
-                "bg-background",
-                className
-            )}
-        >
-            {children}
-        </div>
+  <div className="bg-secondary flex flex-col rounded-2xl p-1">
+    <p className="text-foreground m-0 block shrink-0 px-4 py-2 text-sm font-medium">
+      {title}
+    </p>
+    <div
+      className={cn(
+        "relative flex-1 overflow-hidden rounded-xl border shadow-xs",
+        "bg-background",
+        className
+      )}
+    >
+      {children}
     </div>
+  </div>
 );
