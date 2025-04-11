f27d4536908e73c9a863c0560391b69128021be5
Donny/강동윤 <kdy1997.dev@gmail.com>
2025-04-11 05:34:24 +0000

    build: Use turbopack for build (#283)

--- apps/plugins/next.config.mjs
+++ apps/plugins/next.config.mjs
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental: {
-        typedRoutes: true,
-    },
+  experimental: {},
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
