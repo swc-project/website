a75762730922aea5f7736b3a9847c0887631f730
Niklas Mischkulnig <4586894+mischnic@users.noreply.github.com>
2025-04-11 09:24:49 +0200

    Generate prisma into node_modules

--- apps/plugins/lib/prisma/client.ts
+++ apps/plugins/lib/prisma/client.ts
@@ -1,18 +1,18 @@
-import { PrismaClient } from "@/lib/generated/prisma";
-export { Prisma } from "@/lib/generated/prisma";
+import { PrismaClient } from "@prisma/client";
+export { Prisma } from "@prisma/client";
 
 declare let global: { prisma: PrismaClient };
 
 let prisma: PrismaClient;
 
 if (process.env.NODE_ENV === "production") {
-    prisma = new PrismaClient();
+  prisma = new PrismaClient();
 } else {
-    if (!global.prisma) {
-        global.prisma = new PrismaClient();
-    }
+  if (!global.prisma) {
+    global.prisma = new PrismaClient();
+  }
 
-    prisma = global.prisma;
+  prisma = global.prisma;
 }
 
 export { prisma as db };
